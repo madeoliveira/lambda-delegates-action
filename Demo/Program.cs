e1a07833f19c0cdd985f04e9caf6b42380958875
Marcio Oliveira <marcio.alves78@hotmail.com>
2021-02-15 19:38:39 -0300

    Solution, reference method.

---
+++ Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using Demo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.ForEach(UpdatePrice);
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+    }
+}
