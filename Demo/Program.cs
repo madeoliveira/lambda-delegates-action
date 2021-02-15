e6362876616dc06a25d80c66a754e06dc39752e2
Marcio Oliveira <marcio.alves78@hotmail.com>
2021-02-15 20:05:16 -0300

    Inline lambda expression

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,9 +1,6 @@
 ﻿using Demo.Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Demo
 {
@@ -18,9 +15,7 @@ namespace Demo
             list.Add(new Product("HD Case", 80.90));
 
 
-            Action<Product> act = p => { p.Price += p.Price * 0.1; };
-
-            list.ForEach(act);
+            list.ForEach(p=> { p.Price += p.Price * 0.1; });
             foreach(Product p in list)
             {
                 Console.WriteLine(p);
